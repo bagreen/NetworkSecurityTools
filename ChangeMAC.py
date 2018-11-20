e33e9c5029d5658a331191369aeeec049eae8333
Ben Green <benjaminangusgreen@gmail.com>
2018-11-19 23:55:40 -0800

    Add files via upload

--- ChangeMAC.py
+++ ChangeMAC.py
@@ -1,11 +1,36 @@
-import subprocess
+import optparse, re, subprocess
 
-print('Enter interface, if you don\'t know type unknown')
-interface = input()
+def arguments():
+    parser = optparse.OptionParser()
 
-print('Enter new MAC address')
-newMAC = input()
+    parser.add_option('-i', '--interface', dest = 'interface', help = 'Interface of which the MAC address will be changed')
+    parser.add_option('-m', '--mac', dest = 'newMAC', help = 'New MAC address')
 
-subprocess.call('sudo ifconfig ' + interface + ' down', shell=True)
-subprocess.call('sudo ifconfig ' + interface + ' hw ether ' + newMAC, shell=True)
-subprocess.call('sudo ifconfig ' + interface + ' up', shell=True)
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error('Specify an interface')
+    if not options.newMAC:
+        parser.error('Specify a MAC address')
+def changeMac():
+    subprocess.call('sudo ifconfig ' + options.interface + ' down', shell = True)
+    subprocess.call('sudo ifconfig ' + options.interface + ' hw ether ' + options.newMAC, shell = True)
+    subprocess.call('sudo ifconfig ' + options.interface + ' up', shell = True)
+def getMac():
+    ifconfigResult = subprocess.check_output(['ifconfig', interface])
+    macAddress = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfigResult)
+
+    if macAddress:
+        return macAddress.group(0)
+    else:
+        print('Could not find original MAC address')
+
+arguments()
+changeMac()
+
+currentMac = getMac()
+
+if currentMac == options.newMAC:
+    print('Changed MAC address on ' + options.interface + ' to ' + options.newMAC)
+else:
+    print('Unknown error!')
