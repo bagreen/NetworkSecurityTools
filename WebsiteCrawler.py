346b13685ac14fcc2251591c2dd6a0f02d1ff991
Ben Green <benjaminangusgreen@gmail.com>
2018-11-20 21:23:30 -0800

    Update WebsiteCrawler.py

--- WebsiteCrawler.py
+++ WebsiteCrawler.py
@@ -1,9 +1,34 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import requests
 
-def checkSubdirectories(url):
+
+def checkWebsite(url):
+    checkRequest = requests.get(url)
+
+    if checkRequest.status_code > 400:
+        print('Website is down or doesn\'t exist, try again')
+        checkWebsite()
+    else:
+        print('Website is up')
+def crawlWebsite(url):
+    webRequest = requests.get(url)
+    webSoup = BeautifulSoup(webRequest.text, 'html.parser')
+
+    for link in webSoup.findAll('a'):
+        link = urljoin(url, link.get('href'))
+
+        if '#' in link:
+            link = link.split('#')[0]
+
+        if targetUrl in link and link not in links:
+            links.append(link)
+            print(link)
+            crawlWebsite(link)
+def findSubdirectories(url):
     foundURLs = []
 
-    with open("common.txt", "r") as ifile:
+    with open('common.txt', 'r') as ifile:
         for line in ifile:
             line = line.rstrip()
             subURL = url + '/' + line
@@ -11,23 +36,28 @@ def checkSubdirectories(url):
 
             webRequest = requests.get(subURL)
 
+            print('Checking', subURL)
+
             if webRequest.status_code < 400:
-                print("FOUND " + subURL)
+                print('FOUND ', subURL)
                 foundURLs.append(subURL)
 
     for subURL in foundURLs:
         checkSubdirectories(subURL)
 
-print("Enter website URL you want to find subdirectories for")
+print('Do you want to find subdirectories (s) or crawl website (c)?')
+goal = input()
+
+print('Enter website URL')
 url = input()
 
-checkRequest = requests.get(url)
+checkWebsite(url)
 
-if checkRequest.status_code > 400:
-    print("Website is down or doesn't exist, try again")
-else:
-    print("Website is up")
-    checkSubdirectories(url)
+if goal is 's':
+    findSubdirectories(url)
 
-# print("Enter wordlist you want to use: ")
-# wordlist = input()
+elif goal is 'c':
+    targetUrl = url
+    links = []
+
+    crawlWebsite(url)
