741b811aca42fdda5e1fe130628c75c91c000e65
Ben Green <benjaminangusgreen@gmail.com>
2018-11-27 12:43:11 -0800

    Update ChangeMAC.py

--- ChangeMAC.py
+++ ChangeMAC.py
@@ -1,36 +1,44 @@
-import optparse, re, subprocess
+import argparse
+import re
+import subprocess
 
-def arguments():
-    parser = optparse.OptionParser()
 
-    parser.add_option('-i', '--interface', dest = 'interface', help = 'Interface of which the MAC address will be changed')
-    parser.add_option('-m', '--mac', dest = 'newMAC', help = 'New MAC address')
+def process_arguments():
+    parser = argparse.ArgumentParser(description='Change your MAC address')
+    parser.add_argument('interface')
+    parser.add_argument('mac')
 
-    (options, arguments) = parser.parse_args()
+    parsed = parser.parse_args()
 
-    if not options.interface:
-        parser.error('Specify an interface')
-    if not options.newMAC:
-        parser.error('Specify a MAC address')
-def changeMac():
-    subprocess.call('sudo ifconfig ' + options.interface + ' down', shell = True)
-    subprocess.call('sudo ifconfig ' + options.interface + ' hw ether ' + options.newMAC, shell = True)
-    subprocess.call('sudo ifconfig ' + options.interface + ' up', shell = True)
-def getMac():
-    ifconfigResult = subprocess.check_output(['ifconfig', interface])
-    macAddress = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfigResult)
+    try:
+        return list(vars(parser.parse_args()).values())
+    except IOError:
+        parser.error('Error')
 
-    if macAddress:
-        return macAddress.group(0)
+
+def change_mac(args):
+    subprocess.run(['sudo', 'ifconfig', args[0], 'down'])
+    subprocess.run(['sudo', 'ifconfig', args[0], 'hw', 'ether', args[1]])
+    subprocess.run(['sudo', 'ifconfig', args[0], 'up'])
+
+
+def get_mac(args):
+    ifconfig_result = str(subprocess.check_output(['ifconfig', args[0]]), 'utf-8')
+    mac_address = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
+
+    if mac_address:
+        return mac_address.group(0)
     else:
         print('Could not find original MAC address')
 
-arguments()
-changeMac()
 
-currentMac = getMac()
+arg_results = process_arguments()
 
-if currentMac == options.newMAC:
-    print('Changed MAC address on ' + options.interface + ' to ' + options.newMAC)
+original_mac = get_mac(arg_results)
+change_mac(arg_results)
+current_mac = get_mac(arg_results)
+
+if current_mac == arg_results[1]:
+    print('Changed MAC address on', arg_results[0], 'from', original_mac, 'to', current_mac)
 else:
     print('Unknown error!')
