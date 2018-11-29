94b3a8ea0ddfc198de9117c756f229f7c81ab4c2
Ben Green <benjaminangusgreen@gmail.com>
2018-11-29 00:17:56 -0800

    Update PDF_Decrypter.py

--- PDF_Decrypter.py
+++ PDF_Decrypter.py
@@ -1,11 +1,33 @@
+import argparse
 import subprocess
 
-name = 'test.pdf'
 
-with open('englishDictionary', 'r') as handle:
+def process_arguments():
+    parser = argparse.ArgumentParser(description='Decrypts PDFs using qpdf')
+    parser.add_argument('pdf', help='Name of PDF to decrypt')
+    parser.add_argument('-o', '--output',
+                        help='Name of output file of PDF without password, by default it is \'cracked.pdf\'')
+
+    try:
+        return parser.parse_args()
+    except IOError:
+        parser.error('Error')
+
+
+arguments = process_arguments()
+
+output_file = ''
+
+if arguments.o is not None:
+    output_file = arguments.o
+else:
+    output_file = 'cracked.pdf'
+
+with open('1000MostCommonPasswords.txt', 'r') as handle:
     for line in handle:
-        p = subprocess.Popen(['qpdf', '--password=' + line.strip(), '--decrypt', name, 'cracked.pdf'], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-        output, error = p.communicate()
+        p = subprocess.Popen(['qpdf', '--password=' + line.strip(), '--decrypt', arguments.pdf, output_file],
+                             stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        p.communicate()
 
         if p.returncode == 0:
             print('Password :', line)
