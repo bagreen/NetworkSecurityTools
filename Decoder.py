64695d0e9a559c0d6b81ec3c1c1e16ba05caebe0
Ben Green <benjaminangusgreen@gmail.com>
2018-12-04 13:50:49 -0800

    Added substitution cipher

--- Decoder.py
+++ Decoder.py
@@ -13,8 +13,10 @@ https://simonsingh.net/The_Black_Chamber/vigenere_cracking_tool.html
 from collections import Counter
 import argparse
 import base64
+import copy
 import math
 import re
+import string
 
 # HELPING DICTIONARIES
 letters_to_numbers = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,
@@ -38,9 +40,16 @@ morse_to_english = {'.-...': '&', '--..--': ',', '....-': '4', '.....': '5',
 english_dictionary = {}
 english_frequency = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'W', 'D', 'L', 'Y', 'K', 'C', 'U', 'M', 'F', 'G', 'P',
                      'B', 'V', 'J', 'X', 'Q', 'Z']
+word_patterns = {}
 
 
 # HELPING METHODS
+def blank_mapping():
+    # Returns a dictionary value that is a blank cipherletter mapping:
+    return {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [],
+           'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [],
+           'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [],
+           'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []}
 def frequency_analysis(encoded):
     encoded_list = Counter(encoded).most_common(100)
     frequency_list = []
@@ -77,15 +86,15 @@ def is_english(message):
         return True
     else:
         return False
-def modulo_inverse(a, m):
-    if gcd(a, m) != 1:
+def modulo_inverse(first_num, second_num):
+    if gcd(first_num, second_num) != 1:
         return None  # No mod inverse if a & m aren't relatively prime.
-    u1, u2, u3 = 1, 0, a
-    v1, v2, v3 = 0, 1, m
+    u1, u2, u3 = 1, 0, first_num
+    v1, v2, v3 = 0, 1, second_num
     while v3 != 0:
         q = u3 // v3  # Note that // is the integer division operator.
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
-    return u1 % m
+    return u1 % second_num
 def morse_converter(encoded):
     if len(Counter(encoded).most_common(3)) is 2:
         first_most_common = Counter(encoded).most_common(2)[0][0]
@@ -110,11 +119,11 @@ def morse_encoder(encoded):
     print('English to Morse :', decoded)
 def process_arguments():
     parser = argparse.ArgumentParser(description='Attempts to decrypt input in many different encryption types')
-    parser.add_argument('-i', '--input', help='The information you want to decrypt', default='')
-    parser.add_argument('-v', help='Verbosity prints returned values even if they aren\'t words')
+    parser.add_argument('-i', '--input', help='The information or file you want to decrypt', default='')
+    parser.add_argument('-v', '--verbose', help='Verbosity prints returned values even if they aren\'t words', action='store_true')
 
     try:
-        return list(vars(parser.parse_args()).values())
+        return parser.parse_args()
     except IOError:
         parser.error('Error')
 def rotate(encoded, rotation):
@@ -136,21 +145,31 @@ def setup_dictionary():
     with open('1331811MostCommonEnglishWords.txt', 'r') as file:
         for word in file:
             english_dictionary[word.strip().upper()] = None
+def setup_patterns():
+    fo = open('dictionary.txt')
+    wordList = fo.read().split('\n')
+    fo.close()
 
+    for word in wordList:
+        # Get the pattern for each string in wordList:
+        pattern = word_pattern(word)
 
-# ENCODERS
-def rail_encryption(decoded):
-    rail_input = decoded
+        if pattern not in word_patterns:
+            word_patterns[pattern] = [word]
+        else:
+            word_patterns[pattern].append(word)
+def word_pattern(word):
+    word = word.upper()
+    letter_numbers = {}
+    pattern = []
+    next_num = 0
 
-    for x in range(2, len(decoded)):
-        rail_input = rail_input + decoded
-        encoded = ''
-
-        for y in range(0, len(decoded)):
-            encoded = encoded + rail_input[x * y]
-        print()
-        print('RAIL')
-        print('Shift %s: %s' % (x - 1, decoded))
+    for char in word:
+        if char not in letter_numbers:
+            letter_numbers[char] = str(next_num)
+            next_num += 1
+        pattern.append(letter_numbers[char])
+    return '.'.join(pattern)
 
 
 # DECODERS
@@ -177,7 +196,7 @@ def affine(encoded):
             else:
                 decoded += char
 
-        if is_english(decoded) or verbose:
+        if (decoded != encoded.upper() and is_english(decoded)) or verbose:
             if found is False:
                 found = True
                 print()
@@ -304,17 +323,6 @@ def caesar(encoded):
                 print()
                 print('CAESAR')
             print('Rotated %s: %s' % (rotation, decoded))
-def monoalphabetic(encoded):
-    # Need to figure out how to do this better...
-    encoded = encoded.lower()
-
-    frequency_list = frequency_analysis(encoded)
-
-    for letter in range(0, 25):
-        encoded = encoded.replace(frequency_list[letter], english_frequency[letter])
-    print()
-    print('MONOALPHABETIC')
-    print('Decoded:', encoded)
 def morse(encoded):
     decoded = ''
 
@@ -430,6 +438,80 @@ def reverse(encoded):
         print()
         print('REVERSE')
         print('Reversed :', decoded)
+def substitution(encoded):
+    intersected_map = blank_mapping()
+    partially_decoded = encoded
+    word_list = re.compile('[^A-Z\s]').sub('', encoded.upper()).split()
+
+    for word in word_list:
+        # Get a new word mapping for each encoded word:
+        word_map = blank_mapping()
+
+        word_pat = word_pattern(word)
+        if word_pat not in word_patterns:
+            continue  # This word was not in our dictionary, so continue.
+
+        # Add the letters of each candidate to the mapping:
+        for candidate in word_patterns[word_pat]:
+            for i in range(len(word)):
+                if candidate[i] not in word_map[word[i]]:
+                    word_map[word[i]].append(candidate[i])
+
+        # intersects
+        intersect_map = blank_mapping()
+        for letter in string.ascii_uppercase:
+            if not intersected_map[letter]:
+                intersect_map[letter] = copy.deepcopy(word_map[letter])
+            elif not word_map[letter]:
+                intersect_map[letter] = copy.deepcopy(intersected_map[letter])
+            else:
+                for mappedLetter in intersected_map[letter]:
+                    if mappedLetter in word_map[letter]:
+                        intersect_map[letter].append(mappedLetter)
+        intersected_map = intersect_map
+
+    # removes solved letters from map
+    loopAgain = True
+    while loopAgain:
+        loopAgain = False
+        solvedLetters = []
+        for cipherletter in string.ascii_uppercase:
+            if len(intersected_map[cipherletter]) == 1:
+                solvedLetters.append(intersected_map[cipherletter][0])
+        for cipherletter in string.ascii_uppercase:
+            for s in solvedLetters:
+                if len(intersected_map[cipherletter]) != 1 and s in intersected_map[cipherletter]:
+                    intersected_map[cipherletter].remove(s)
+                    if len(intersected_map[cipherletter]) == 1:
+                        loopAgain = True
+
+    # now we have complete mapping, so let's decrypt
+    key = ['x'] * 26
+    for cipherletter in string.ascii_uppercase:
+        if len(intersected_map[cipherletter]) == 1:
+            # If there's only one letter, add it to the key.
+            keyIndex = string.ascii_uppercase.find(intersected_map[cipherletter][0])
+            key[keyIndex] = cipherletter
+        else:
+            partially_decoded = partially_decoded.replace(cipherletter.lower(), '_')
+            partially_decoded = partially_decoded.replace(cipherletter.upper(), '_')
+    key = ''.join(key)
+
+    # decode message
+    decoded = ''
+    for char in partially_decoded:
+        if char.upper() in key:
+            # Encrypt/decrypt the symbol:
+            index = key.find(char.upper())
+            if char.isupper():
+                decoded += string.ascii_uppercase[index].upper()
+            else:
+                decoded += string.ascii_uppercase[index].lower()
+        else:
+            decoded += char
+    print()
+    print('SUBSTITUTION')
+    print('Decoded:', decoded)
 def transposition(encoded):
     if len(encoded) > 2:
         found = False
@@ -507,11 +589,16 @@ def vigenere(encoded):
 '''
 
 
-setup_dictionary()
+arguments = process_arguments()
+encoded_text = arguments.input
+verbose = arguments.verbose
 
-encoded_text = process_arguments()[0]
-# encoded_text = 'anbk bii berjg zg'
-verbose = process_arguments()[1]
+setup_dictionary()
+setup_patterns()
+
+# encoded_text = process_arguments()[0]
+# encoded_text = 'Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr sxrjsxwjr, ia esmm rwctjsxsza sj wmpramh, lxo txmarr jia aqsoaxwa sr pqaceiamnsxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac ilxo, ia sr pyyacao rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx lwwpcolxwa jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh. -Facjclxo Ctrramm'
+# verbose = process_arguments()[1]
 # verbose = True
 
 print('(Press Ctrl+D to quit at anytime)')
@@ -536,4 +623,5 @@ null(encoded_text)
 number_base(encoded_text)
 polybius(encoded_text)
 reverse(encoded_text)
+substitution(encoded_text)
 transposition(encoded_text)
